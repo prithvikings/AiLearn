946f41ebd804a2a4835752de6088ff95986b5fe9
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2026-09-15 18:43:00 +0530

    fix: load celebration css as stylesheet

--- js/phase4-sync.js
+++ js/phase4-sync.js
@@ -2,7 +2,14 @@ import { loadState } from "./state.js";
 import "./journey.js";
 import "./gamification-ui.js";
 import "./celebration.js";
-import "../css/celebration.css";
+
+if (!document.querySelector("link[data-celebration-styles]")) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "css/celebration.css";
+  link.dataset.celebrationStyles = "true";
+  document.head.appendChild(link);
+}
 
 let signature = JSON.stringify(loadState());
 
