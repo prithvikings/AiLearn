c435125fc18c1e8f371608355f5e933812b8b72d
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2026-09-15 19:05:46 +0530

    feat: wire achievement feedback into shared sync

--- js/phase4-sync.js
+++ js/phase4-sync.js
@@ -3,6 +3,7 @@ import "./journey.js";
 import "./gamification-ui.js";
 import "./celebration.js";
 import "./mastery.js";
+import "./achievement-feedback.js";
 
 if (!document.querySelector("link[data-celebration-styles]")) {
   const link = document.createElement("link");
