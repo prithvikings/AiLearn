23369058a448adc731f44379f6768f8f1fa276e1
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2026-09-15 18:42:49 +0530

    feat: load celebration accent styles

--- js/phase4-sync.js
+++ js/phase4-sync.js
@@ -2,6 +2,7 @@ import { loadState } from "./state.js";
 import "./journey.js";
 import "./gamification-ui.js";
 import "./celebration.js";
+import "../css/celebration.css";
 
 let signature = JSON.stringify(loadState());
 
